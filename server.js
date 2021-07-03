d5d1cc58c08644edb2571d9d2837074b6de44ab2
Trevor Wiegand <trevorwiegand92@gmail.com>
2021-07-02 21:30:55 -0500

    added the db, models, routes, and seeds folders and corresponding files

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const sequelize = require('./config/connection');
+const express = require("express");
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,5 +8,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
+});
+
+const express = require("express");
+const routes = require("./routes");
+// import sequelize connection
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
 });
